dce893c6cee1f8802673149cd938c6bedbd5cb8b
Sanjay-Kumar-S-GitHub <sanjay.k.sundararajan@gmail.com>
2025-02-10 21:32:05 +0530

    backgrd image added to home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Navbar from "./Components/Navbar/Navbar";
+import Navbar from "./Components/homePage/navbar";
+import TopComponnent from "./Components/homePage/Top";
 
 const App = () => {
   return (
     <div className="overflow-x-hidden">
       <Navbar />
+      <TopComponnent />
     </div>
   );
 };
---
+++ Frontend/src/Components/homePage/Top.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function TopComponnent(){
+    return (
+        <>
+            <div>
+                <div className="flex  bg-[url('images/home/top.jpg')] h-screen w-full bg-no-repeat bg-cover bg-center" >
+                    <div>
+                        <h1 className="font-bold text-8xl">Your wedding team and everything in between</h1>
+                    </div>
+                    
+                </div>
+            </div>
+        </>
+    );
+};
