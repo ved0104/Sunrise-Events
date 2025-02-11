bcc353a0667a73584a7bc7a37110bf45a4448a48
Vedang Dubey <dubeyved04@gmail.com>
2025-02-11 12:39:01 +0530

    Added footer

--- Frontend/src/Components/final/FinalComponent.jsx
+++ Frontend/src/Components/final/FinalComponent.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import TopComponnent from '../homePage/Top'
+import Footer from '../Footer'
 
 const FinalComponent = () => {
   return (<>
     <Navbar/>
     <TopComponnent/>
+    <Footer/>
     </>
   )
 }
