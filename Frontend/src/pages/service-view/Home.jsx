7f2c67a6932b5e5b530efd6115bc0e18997ddabf
Vedang Dubey <dubeyved04@gmail.com>
2025-02-12 17:55:04 +0530

    edited home

--- Frontend/src/pages/service-view/Home.jsx
+++ Frontend/src/pages/service-view/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import Footer from '../../Components/Footer/Footer'
 import TopComponnent from '../../Components/homePage/Top'
 import ServiceComponent from '../../Components/homePage/ServiceComponent'
 import HeroSection from "../../Components/homePage/HeroSection"
 import FramerMotion from "../../Components/homePage/FreamerMotion"
+import HoverCard from '../../Components/homePage/ServiceHoverCard'
+import Instagram from '../../Components/homePage/Instagram'
+import EndPart from '../../Components/homePage/End'
 const home = () => {
   return (<>
     <TopComponnent/>
     <HeroSection/>
     <FramerMotion/>
     <ServiceComponent/>
-    </>
+    <HoverCard/>
+    <EndPart/>
+    <Instagram/>
+</>
   )
 }
 
