d4280b98b9cf7f8dc02c142b0066bb2d33ca5c56
Vedang Dubey <dubeyved04@gmail.com>
2025-05-19 12:12:44 +0530

    resolved bg images path in deploy

--- Frontend/src/Components/homePage/Top.jsx
+++ Frontend/src/Components/homePage/Top.jsx
@@ -7,7 +7,7 @@ export default function TopComponent() {
   return (
     <div className="min-h-100 sm:min-h-140 flex flex-col justify-center items-center pt-20 pb-8">  
       <div
-        className="text-white flex flex-col items-center pt-11 bg-[url('src/assets/images/home/top6.jpg')] md:bg-[url('src/assets/images/home/top2.jpg')] bg-cover bg-center bg-no-repeat min-h-100 sm:min-h-140 min-w-full"
+        className="text-white flex flex-col items-center pt-11 bg-[url('/images/home/top6.jpg')] md:bg-[url('/images/home/top2.jpg')] bg-cover bg-center bg-no-repeat min-h-100 sm:min-h-140 min-w-full"
       >
         <div className="text-center mb-6 px-2 sm:px-4">
           <h1 className="font-inter font-extrabold text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-3d-effect drop-shadow-[4px_4px_0px_rgba(0,0,0,0.5)] sm:drop-shadow-[6px_6px_0px_rgba(0,0,0,0.6)] md:drop-shadow-[8px_8px_0px_rgba(0,0,0,0.7)]">
